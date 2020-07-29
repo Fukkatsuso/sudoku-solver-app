377534010824749b08aad66f5d701017da7b2efb
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-07-29 19:38:17 +0900

    不要なコードの削除

--- main.go
+++ main.go
@@ -11,29 +11,6 @@ import (
 	"github.com/Fukkatsuso/sudoku"
 )
 
-// 数字文字列を9x9のテーブルに変換
-func stringToTable(s string) [][]int {
-	t := make([][]int, 9)
-	for i := 0; i < 9; i++ {
-		t[i] = make([]int, 9)
-		for j := 0; j < 9; j++ {
-			t[i][j] = int(s[i*9+j] - '0')
-		}
-	}
-	return t
-}
-
-// 9x9のテーブルを文字列に変換
-func tableToString(t [][]int) string {
-	bs := make([]byte, 9*9)
-	for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
-			bs[i*9+j] = byte('0' + t[i][j])
-		}
-	}
-	return string(bs)
-}
-
 func index(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("views/index.html"))
 	if err := tmpl.Execute(w, nil); err != nil {
